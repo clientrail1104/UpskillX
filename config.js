1d86213bd43995290940f8bc9940a831a6906b87
clientrail1104 <clientrail@gmail.com>
2026-08-16 16:39:29 +0800

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-// Put ONLY your Supabase project URL and publishable/anon key here.
-// Never put a service_role or secret key in this file.
-window.SUPABASE_URL="YOUR_SUPABASE_PROJECT_URL";
-window.SUPABASE_PUBLISHABLE_KEY="YOUR_SUPABASE_PUBLISHABLE_KEY";
